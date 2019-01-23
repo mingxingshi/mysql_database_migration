6c210b1adba1797bf764c2e0525d19388e743fed
mingxingshi <95818584@qq.com>
2019-01-23 09:28:46 +0800

    Update database_migration.sh

--- database_migration.sh
+++ database_migration.sh
@@ -3,7 +3,7 @@
 
 if [ $# -ne 7 ];then
     echo "Usage: `basename $0` <srcdb_host> <srcdb_user> <srcdb_password> <db_name> <dstdb_host> <dstdb_user> <dstdb_password>"
-	exit 2
+    exit 2
 fi
 
 HOST=$1
@@ -21,7 +21,7 @@ export MYSQL_PWD=$3
 mysql -h${db_host} -u${db_user} -s -e "select 1;" 1>/dev/null 2>&1
 if [ X"$?" != X"0" ];then
     echo "Connect to database ${db_host} failed"
-	exit 1
+    exit 1
 fi
 }
 test_database_connectivity ${HOST} ${USER} $3
@@ -30,10 +30,10 @@ test_database_connectivity ${DST_HOST} ${DST_USER} $7
 rm -f ${BACKUPDIR}/${DATABASE}_ddl.sql ${BACKUPDIR}/${DATABASE}_dml.sql
 if [ ! -f ${BACKUPDIR}/${DATABASE}.txt ];then
     echo "Not found {DATABASE}.txt"
-	exit 1
+    exit 1
 elif [ X`which mysql mysqldump 1>/dev/null 2>&1 && echo $?` != X"0" ];then
     echo "Not found mysql client"
-	exit 1
+    exit 1
 fi
 
 #导出
@@ -42,30 +42,30 @@ CREATE_DATABASE_SQL=`mysql -h${HOST} -u${USER} -s -e "select concat('CREATE DATA
 
 for table in `awk -F"," '{print $1}' ${BACKUPDIR}/${DATABASE}.txt`;do
     mysqldump -h ${HOST} -u ${USER} -q -d ${DATABASE} ${table} >> ${BACKUPDIR}/${DATABASE}_${table}_ddl.sql
-	if [ X"$?" == X"0" ];then
+    if [ X"$?" == X"0" ];then
         echo "Export structure of table $table successfully."
-		cat ${BACKUPDIR}/${DATABASE}_${table}_ddl.sql >> ${BACKUPDIR}/${DATABASE}_ddl.sql && rm -f ${BACKUPDIR}/${DATABASE}_${table}_ddl.sql
-		if [ `awk -F"," '{if($1=="'${table}'") print $2}' ${BACKUPDIR}/${DATABASE}.txt` == "dml" ]; then
-			mysqldump -h ${HOST} -u ${USER} -q -t ${DATABASE} ${table} >> ${BACKUPDIR}/${DATABASE}_${table}_dml.sql
-			if [ X"$?" == X"0" ];then
-			    echo "Export data of table ${table} successfully."
-				cat ${BACKUPDIR}/${DATABASE}_${table}_dml.sql >> ${BACKUPDIR}/${DATABASE}_dml.sql && rm -f ${BACKUPDIR}/${DATABASE}_${table}_dml.sql
-			else
-			    echo "Export data of table ${table} failed."
-				rm -f ${BACKUPDIR}/${DATABASE}_${table}_dml.sql
-				echo "$table,export data failed" >> ${DATABASE}_error.log
-			fi
-		fi
+        cat ${BACKUPDIR}/${DATABASE}_${table}_ddl.sql >> ${BACKUPDIR}/${DATABASE}_ddl.sql && rm -f ${BACKUPDIR}/${DATABASE}_${table}_ddl.sql
+        if [ `awk -F"," '{if($1=="'${table}'") print $2}' ${BACKUPDIR}/${DATABASE}.txt` == "dml" ]; then
+            mysqldump -h ${HOST} -u ${USER} -q -t ${DATABASE} ${table} >> ${BACKUPDIR}/${DATABASE}_${table}_dml.sql
+            if [ X"$?" == X"0" ];then
+                echo "Export data of table ${table} successfully."
+                cat ${BACKUPDIR}/${DATABASE}_${table}_dml.sql >> ${BACKUPDIR}/${DATABASE}_dml.sql && rm -f ${BACKUPDIR}/${DATABASE}_${table}_dml.sql
+            else
+                echo "Export data of table ${table} failed."
+                rm -f ${BACKUPDIR}/${DATABASE}_${table}_dml.sql
+                echo "$table,export data failed" >> ${DATABASE}_error.log
+            fi
+        fi
     else
         echo "Export structure of table $table failed."
-		rm -f ${BACKUPDIR}/${DATABASE}_${table}_ddl.sql
-		echo "$table,export stucture failed" >> ${DATABASE}_error.log
-		continue
+        rm -f ${BACKUPDIR}/${DATABASE}_${table}_ddl.sql
+        echo "$table,export stucture failed" >> ${DATABASE}_error.log
+        continue
     fi
-	mysqldump -h ${HOST} -u ${USER} -ntd -R ${DATABASE} >> ${BACKUPDIR}/${DATABASE}_ddl.sql
-	if [ X"$?" == X"0" ];then
-	    echo "Export functions and procedures successfully"
-	fi
+    mysqldump -h ${HOST} -u ${USER} -ntd -R ${DATABASE} >> ${BACKUPDIR}/${DATABASE}_ddl.sql
+    if [ X"$?" == X"0" ];then
+        echo "Export functions and procedures successfully"
+    fi
 done
 
 #导入目标库
@@ -74,19 +74,19 @@ mysql -h${DST_HOST} -u${DST_USER} -s -e "${CREATE_DATABASE_SQL}" 1>/dev/null 2>&
 if [ X"$?" == X"0" ];then
     mysql -h${DST_HOST} -u${DST_USER} ${DATABASE} < ${BACKUPDIR}/${DATABASE}_ddl.sql
     if [ X"$?" == X"0" ];then
-	    echo "Import structure to target database successfully";
+        echo "Import structure to target database successfully";
         mysql -h${DST_HOST} -u${DST_USER} ${DATABASE} < ${BACKUPDIR}/${DATABASE}_dml.sql
-		if [ X"$?" == X"0" ];then
-		    echo "Import data to target database successfully";
-		else
-			echo "Import data to target database failed"
-			exit 1
-		fi
+        if [ X"$?" == X"0" ];then
+            echo "Import data to target database successfully";
+        else
+            echo "Import data to target database failed"
+            exit 1
+        fi
     else
         echo "Import structure to target database failed"
         exit 1
     fi
 else
     echo "Create database ${DATABASE} failed"
-	exit 1
+    exit 1
 fi
